9b705e00c2cea7c25992918bbb85dedd7c9ba8f9
Dieter Baron <dillo@nih.at>
2023-12-21 23:11:30 +0100

    Use correct include file.

--- lib/Util.h
+++ lib/Util.h
@@ -32,12 +32,11 @@ OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
 IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
+#include <cstdarg>
+#include <filesystem>
 #include <string>
 
-#include <cstdarg>
-
 #include "printf_like.h"
-#include <__filesystem/filesystem_error.h>
 
 std::string string_format(const char *format, ...) PRINTF_LIKE(1, 2);
 std::string string_format_v(const char *format, va_list ap);
