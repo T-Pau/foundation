0615162716e54539e50f36eca2a90b5c946c00a3
Dieter Baron <dillo@nih.at>
2026-05-19 16:02:13 +0200

    Add Symbol tests.

---
+++ tests/test-symbol.cc
@@ -0,0 +1,48 @@
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers.hpp>
+
+#include "Symbol.h"
+
+
+TEST_CASE("Symbol: empty", "[symbol]") {
+    auto empty = tpau::cpp_kernal::Symbol();
+    REQUIRE(empty.str() == "");
+    REQUIRE(strcmp(empty.c_str(), "") == 0);
+    REQUIRE(!empty);
+    REQUIRE(empty.empty());
+}
+
+TEST_CASE("Symbol: non-empty", "[symbol]") {
+    auto a = tpau::cpp_kernal::Symbol("a");
+    REQUIRE(a.str() == "a");
+    REQUIRE(strcmp(a.c_str(), "a") == 0);
+    REQUIRE(a);
+    REQUIRE(!a.empty());
+    REQUIRE(a != tpau::cpp_kernal::Symbol());
+}
+
+TEST_CASE("Symbol: same", "[symbol]") {
+    auto a1 = tpau::cpp_kernal::Symbol("a");
+    auto a2 = tpau::cpp_kernal::Symbol("a");
+    REQUIRE(a1 == a2);
+    REQUIRE(a1 <= a2);
+    REQUIRE(a1 >= a2);
+    REQUIRE(!(a1 < a2));
+    REQUIRE(!(a1 > a2));
+    REQUIRE(!(a1 != a2));
+}
+
+TEST_CASE("Symbol: different", "[symbol]") {
+    auto a = tpau::cpp_kernal::Symbol("a");
+    auto b = tpau::cpp_kernal::Symbol("b");
+    REQUIRE(a != b);
+    REQUIRE(a < b);
+    REQUIRE(a <= b);
+    REQUIRE(b > a);
+    REQUIRE(b >= a);
+    REQUIRE(!(a == b));
+    REQUIRE(!(a > b));
+    REQUIRE(!(a >= b));
+    REQUIRE(!(b < a));
+    REQUIRE(!(b <= a));
+}
